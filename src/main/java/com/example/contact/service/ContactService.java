1ee629c4822eddb06ddbec1efc2467dd65f36a45
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-22 21:22:34 -0700

    feat(contact): added get all contacts method to service layer feat(contact):added CRUD methods to service layer feat(contact): added ability to upload photo for contact

--- src/main/java/com/example/contact/service/ContactService.java
+++ src/main/java/com/example/contact/service/ContactService.java
@@ -1,4 +1,78 @@
 package com.example.contact.service;
 
+import com.example.contact.model.Contact;
+import com.example.contact.repository.ContactRepo;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static com.example.contact.constant.Constant.PHOTO_DIRECTORY;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
 public class ContactService {
+    private final ContactRepo contactRepo;
+
+    public Page<Contact> getAllContacts(int page, int size){
+        return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+
+    public Contact getContact(String id){
+        return contactRepo.findById(id).orElseThrow(()-> new RuntimeException("Contact Not Found"));
+    }
+
+    public Contact createContact(Contact contact){
+        return contactRepo.save(contact);
+    }
+
+    public void deleteContact(Contact contact){
+        contactRepo.delete(contact);
+    }
+
+    public String uploadPhoto(String id, MultipartFile file){
+        Contact contact = getContact(id);
+        String photoUrl =photoFunction.apply(id,file) ;
+        contact.setPhotoUrl(photoUrl);
+        contactRepo.save(contact);
+        return photoUrl;
+    }
+
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
+            .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String fileName = id + fileExtension.apply(image.getOriginalFilename());
+        try {
+
+            Path fileStorage = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+
+            if (!Files.exists(fileStorage)) {
+                Files.createDirectories(fileStorage);
+            }
+
+            Files.copy(image.getInputStream(), fileStorage.resolve(fileName), REPLACE_EXISTING);
+
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath().path("/contacts/image/" + fileName).toUriString();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Unable To Save Image");
+        }
+    };
 }
