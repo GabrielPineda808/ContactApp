ec9a4ca3caa73ee1b6b2a7055af1c09a667c6bb6
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-22 21:18:43 -0700

    feat(repo): add BookingRepository with standard CRUD

--- src/main/java/com/example/contact/repository/ContactRepo.java
+++ src/main/java/com/example/contact/repository/ContactRepo.java
@@ -1,4 +1,13 @@
 package com.example.contact.repository;
 
-public class ContactRepo {
+import com.example.contact.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
 }
