166f971eb426ab710e857b52236fac514672c174
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-23 22:10:40 -0700

    feat(contact): created endpoint for creating contact

--- src/main/java/com/example/contact/controller/ContactController.java
+++ src/main/java/com/example/contact/controller/ContactController.java
@@ -1,9 +1,24 @@
 package com.example.contact.controller;
 
+import com.example.contact.model.Contact;
+import com.example.contact.service.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
-@RequestMapping
+@RequestMapping("/contacts")
+@RequiredArgsConstructor
 public class ContactController {
+    private final ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact){
+        return ResponseEntity.created(URI.create("/contacts/<user_ID>")).body(contactService.createContact(contact));
+    }
 }
