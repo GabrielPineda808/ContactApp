4a69bd77b7bd5bc1b25882c6559dbf18977556a7
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-24 17:17:16 -0700

    deat(contact): added endpoint to get all contacts which returns a paginated result

--- src/main/java/com/example/contact/controller/ContactController.java
+++ src/main/java/com/example/contact/controller/ContactController.java
@@ -3,11 +3,9 @@ package com.example.contact.controller;
 import com.example.contact.model.Contact;
 import com.example.contact.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -21,4 +19,12 @@ public class ContactController {
     public ResponseEntity<Contact> createContact(@RequestBody Contact contact){
         return ResponseEntity.created(URI.create("/contacts/<user_ID>")).body(contactService.createContact(contact));
     }
+
+    @GetMapping
+    public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value="page", defaultValue="0") int page,
+                                                     @RequestParam(value = "size", defaultValue = "10") int size){
+        return ResponseEntity.ok().body(contactService.getAllContacts(page,size));
+    }
+
+
 }
