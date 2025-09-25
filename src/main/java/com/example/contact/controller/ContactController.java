6e54df1e70b8a5055bc95154fadc196c3bfd0136
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-24 17:58:11 -0700

    feat(contact): endpoint to serve image file over http

--- src/main/java/com/example/contact/controller/ContactController.java
+++ src/main/java/com/example/contact/controller/ContactController.java
@@ -8,7 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.example.contact.constant.Constant.PHOTO_DIRECTORY;
 
 @RestController
 @RequestMapping("/contacts")
@@ -35,6 +40,11 @@ public class ContactController {
     @PutMapping("/photo")
     public ResponseEntity<String> uploadPhoto(@RequestParam("id")String id, @RequestParam("file")MultipartFile file){
         return ResponseEntity.ok().body(contactService.uploadPhoto(id,file));
+    }
+
+    @GetMapping("/image/{filename}")
+    public byte[] getPhoto(@PathVariable("filename") String filename) throws IOException {
+        return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY + filename));
     }
 
     @DeleteMapping
