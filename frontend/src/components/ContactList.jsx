cbc55601bf940c21dc7ad2c67af8ffd9d91acead
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-27 16:26:55 -0700

    feat(contactList): added pagination element using the getAllContacts funtion passed in as prop and json data from api allowing user to click through contact list

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -8,6 +8,20 @@ const ContactList = ({data, currentPage, getAllContacts }) => { // takes in all 
         <ul className='contact__list'>
             {data?.content?.length > 0 && data.content.map(contact => <Contact contact={contact} key={contact.id}/>)} //if length of content is greater than 0, map through each contact and display it
         </ul>
+
+        {data?.content?.length > 0 && data?.totalPages > 1 &&
+            <div className='pagination'>
+                <a onClick={()=> getAllContacts(currentPage - 1)} className={0 === currentPage ? 'disabled' : ''}> // go backwards on pagination, disable if on first page
+                    &laquo; //left double angle quote
+                </a>
+
+                {[...Array(data.totalPages)].keys().map((page, index) =>
+                     <a onClick={getAllContacts(page)} className={currentPage === page ? 'active' : ''} key={page} >{page + 1 }</a>)}
+
+                <a onClick={()=> getAllContacts(currentPage + 1)} className={data.totalPages === currentPage + 1 ? 'disabled' : ''}> // go forwards on pagination, disable if on last page  
+                    &raquo; //right double angle quote
+                </a>
+            </div>}
     </main>
   )
 }
