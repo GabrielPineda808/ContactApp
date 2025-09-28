3b73408fccd310401590d630c6f8c2ba14000f8a
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-27 17:05:37 -0700

    feat(main app): set state and created function to get contacts on first render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { getContacts } from './services/contactsService'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const getAllContacts = async (page = 0, size = 10) => {
+    try {
+      setCurrentPage(page);
+      const {data} = await getContacts(page, size);
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching contacts:', error);
+    }
+  }
+
+  useEffect(() => { getAllContacts();}, []); // runs once when componenet is reandered if there is no [] then it runs after every render and if there is an object inside the [] it wil run after first render and when that obj changes only
+
   return (
     <>
       <div>
