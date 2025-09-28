54c6d8237a49ac805bfcde437bd05636cfb65a98
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-27 17:36:16 -0700

    chore(comments): changed comment formatting to now show on render

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -4,22 +4,22 @@ import Contact from './Contact'
 const ContactList = ({data, currentPage, getAllContacts }) => { // takes in all data from backend aka the page we get from our backend of contact lists
   return (
     <main className='main'>
-        { data?.content?.length === 0 && <div>No Contacts. Please add  a new contact.</div>} //if length of content is 0, display no contacts data and content may not be present
+        { data?.content?.length === 0 && <div>No Contacts. Please add  a new contact.</div>} {/*if length of content is 0, display no contacts data and content may not be present*/}
         <ul className='contact__list'>
-            {data?.content?.length > 0 && data.content.map(contact => <Contact contact={contact} key={contact.id}/>)} //if length of content is greater than 0, map through each contact and display it
+            {data?.content?.length > 0 && data.content.map(contact => <Contact contact={contact} key={contact.id}/>)} {/*if length of content is greater than 0, map through each contact and display it*/}
         </ul>
 
         {data?.content?.length > 0 && data?.totalPages > 1 &&
             <div className='pagination'>
-                <a onClick={()=> getAllContacts(currentPage - 1)} className={0 === currentPage ? 'disabled' : ''}> // go backwards on pagination, disable if on first page
-                    &laquo; //left double angle quote
+                <a onClick={()=> getAllContacts(currentPage - 1)} className={0 === currentPage ? 'disabled' : ''}> {/* go backwards on pagination, disable if on first page*/}
+                    &laquo; {/*left double angle quote*/}
                 </a>
 
                 {[...Array(data.totalPages)].keys().map((page, index) =>
                      <a onClick={getAllContacts(page)} className={currentPage === page ? 'active' : ''} key={page} >{page + 1 }</a>)}
 
-                <a onClick={()=> getAllContacts(currentPage + 1)} className={data.totalPages === currentPage + 1 ? 'disabled' : ''}> // go forwards on pagination, disable if on last page  
-                    &raquo; //right double angle quote
+                <a onClick={()=> getAllContacts(currentPage + 1)} className={data.totalPages === currentPage + 1 ? 'disabled' : ''}> {/* go forwards on pagination, disable if on last page */}
+                    &raquo; {/*right double angle quote*/}
                 </a>
             </div>}
     </main>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Header = ({toggleModal, nbOfContacts}) => { //passing props from App.jsx function for header we will put in app.jsx
+const Header = ({toggleModal, nbOfContacts}) => { /*passing props from App.jsx function for header we will put in app.jsx*/
   return (
-    <header className='header'> // header component with bootstrap
+    <header className='header'> {/*header component with bootstrap*/}
         <div className="container">
-            <h3>Contact List ({nbOfContacts})</h3> //nbOfContacts is a prop
+            <h3>Contact List ({nbOfContacts})</h3> {/*nbOfContacts is a prop */}
             <button onClick={()=> toggleModal(true)} className='btn'>
               <i className='bi bi-plus-square'></i> Add New Contact
             </button>
