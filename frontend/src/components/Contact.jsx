a5b90356ca42bb8b8cd061d1586ab7178736998e
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-27 17:49:07 -0700

    bug(contact): email was not parsing due to calling on wrong element of data

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Contact = ({ contact }) => { //takes in contact prop
   return (
-    <Link to={`/contacts/${contact.id}`} className='contact__item'> //routes to contact id card page in future
+    <Link to={`/contacts/${contact.id}`} className='contact__item'> {/*routes to contact id card page in future*/}
         <div className='contact__header'>
             <div className='contact__image'>
                 <img src={contact.photoUrl} alt={contact.name}></img>
@@ -14,11 +14,11 @@ const Contact = ({ contact }) => { //takes in contact prop
             </div>
         </div>
         <div className='contact__body'>
-            <p><i className='bi bi-envelope'></i> {contact.emai.substring(0,20)}</p>
+            <p><i className='bi bi-envelope'></i> {contact.email.substring(0,20)}</p>
             <p><i className='bi bi-geo'></i> {contact.address}</p>
             <p><i className='bi bi-telephone'></i> {contact.phone}</p>
             <p>{contact.status==='Active' ? <i className='bi bi-check-circle'></i> : 
-            <i className='bi bi-x-circle'></i>} {contact.status}</p> //if statement to pass in icon on contact prop status
+            <i className='bi bi-x-circle'></i>} {contact.status}</p> {/* if statement to pass in icon on contact prop status */}
         </div>
     </Link>
   )
