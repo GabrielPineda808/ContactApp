782c56c185b133651e8098e1d8b2a448ec3f041e
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-27 18:02:09 -0700

    feature(main): added modal element that opens to take in new contact info. Currently cancel and save do not work but you can open and close modal with icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Header from './components/Header'
 import ContactList from './components/ContactList'
 import { getContacts } from './api/ContactService'
@@ -6,6 +6,7 @@ import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
+  const modalRef = useRef();
   const [data, setData] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
 
@@ -20,7 +21,7 @@ function App() {
     }
   }
 
-  const toggleModal = (show) => {}
+  const toggleModal = (show) => show ? modalRef.current.showModal() : modalRef.current.close(); // toggle modal natively with html of ref 
 
   useEffect(() => { getAllContacts();}, []); // runs once when componenet is reandered if there is no [] then it runs after every render and if there is an object inside the [] it wil run after first render and when that obj changes only
 
@@ -30,11 +31,58 @@ function App() {
       <main className='main'>
         <div className='container'>
           <Routes>
-            <Route path='/' element={<Navigate  to='/contacts'/>}></Route>
+            <Route path='/' element={<Navigate  to='/contacts'/>}></Route>SS
             <Route path='/contacts' element={<ContactList data={data} currentPage={currentPage} getAllContacts={getAllContacts} />}/>
           </Routes>
         </div>
       </main>
+
+       {/* Modal */}
+      <dialog ref={modalRef} className="modal" id="modal">
+        <div className="modal__header">
+          <h3>New Contact</h3>
+          <i onClick={() => toggleModal(false)} className="bi bi-x-lg"></i>
+        </div>
+        <div className="divider"></div>
+        <div className="modal__body">
+          <form>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Name</span>
+                <input type="text"  name='name' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input type="text"  name='email' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Title</span>
+                <input type="text"  name='title' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Phone Number</span>
+                <input type="text"  name='phone' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Address</span>
+                <input type="text"  name='address' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Account Status</span>
+                <input type="text"  name='status' required />
+              </div>
+              <div className="file-input">
+                <span className="details">Profile Photo</span>
+                <input type="file" name='photo' required />
+              </div>
+            </div>
+            <div className="form_footer">
+              <button type='button' className="btn btn-danger">Cancel</button>
+              <button type='submit' className="btn">Save</button>
+            </div>
+          </form>
+        </div>
+      </dialog>
     </>
   )
 }
