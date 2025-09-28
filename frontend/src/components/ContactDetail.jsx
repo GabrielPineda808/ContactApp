b2e0c179cb303f8f59d8c2e79aea6c8ffe1b0f47
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-27 19:45:38 -0700

    feat(contactDetail): added form with previous contact info for user to update contact

--- frontend/src/components/ContactDetail.jsx
+++ frontend/src/components/ContactDetail.jsx
@@ -41,6 +41,20 @@ const ContactDetail = ({updateContact, updateImage}) => {
         };
     }
 
+    const onChange = (event) => {
+        setContact({ ...contact, [event.target.name]: event.target.value }); // spread operator to copy existing values and update the specific field aka set input values as current contact you are editing
+    };
+
+    const onUpdateContact = async (event) => {
+        event.preventDefault(); // prevent form from submitting and reloading the page
+        await updateContact(contact);// call updateContact function from props   
+        fetchContact(id); // refresh contact details
+    };
+
+    useEffect(() => {
+        fetchContact(id); // fetch contact details when component mounts or id changes
+    }, []);
+
     
     const selectImage = () => {
         inputRef.current.click(); // programmatically click the hidden file input
@@ -60,7 +74,42 @@ const ContactDetail = ({updateContact, updateImage}) => {
                 <button onClick={selectImage} className='btn'><i className='bi bi-cloud-upload'></i>Change Photo</button>
             </div>
         </div>
-        <div className='profile__settings'>Settings</div>
+        <div className='profile__settings'>
+            <div>
+                 <form onSubmit={onUpdateContact} className="form">
+                            <div className="user-details">
+                                <input type="hidden" defaultValue={contact.id} name="id" required /> {/* hidden input to hold contact id */}
+                                <div className="input-box">
+                                    <span className="details">Name</span>
+                                    <input type="text" value={contact.name} onChange={onChange} name="name" required />
+                                </div>
+                                <div className="input-box">
+                                    <span className="details">Email</span>
+                                    <input type="text" value={contact.email} onChange={onChange} name="email" required />
+                                </div>
+                                <div className="input-box">
+                                    <span className="details">Phone</span>
+                                    <input type="text" value={contact.phone} onChange={onChange} name="phone" required />
+                                </div>
+                                <div className="input-box">
+                                    <span className="details">Address</span>
+                                    <input type="text" value={contact.address} onChange={onChange} name="address" required />
+                                </div>
+                                <div className="input-box">
+                                    <span className="details">Title</span>
+                                    <input type="text" value={contact.title} onChange={onChange} name="title" required />
+                                </div>
+                                <div className="input-box">
+                                    <span className="details">Status</span>
+                                    <input type="text" value={contact.status} onChange={onChange} name="status" required />
+                                </div>
+                            </div>
+                            <div className="form_footer">
+                                <button type="submit" className="btn">Save</button>
+                            </div>
+                        </form>
+            </div>
+        </div>
     </div>
 
     <form style={{display: 'none'}}>
